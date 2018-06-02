4fbda51fd1eb73fe1bccef139a22f887ed56c841
maxinakenty <mgreen097@gmail.com>
2018-06-02 22:31:08 +0300

    fix bugs

--- tools/webpack.production.js
+++ tools/webpack.production.js
@@ -23,18 +23,6 @@ module.exports = {
   },
   watch: false,
   devtool: false,
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
-  },
   plugins: [
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify('production'),
